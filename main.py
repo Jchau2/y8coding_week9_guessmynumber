7694961651d2b1882eb53d87cc53de9364e5613f
Jchau2 <jchau@kellettschool.com>
2024-11-06 04:40:11 +0000

    Change1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# 
+
+from random import randint 
+
+correct = randint(16, 64) 
+
+number = int(input("Guess my number >:)"))
+
+while number != correct:
+
+    if number > correct:
+        print("LOWER!")
+    if number < correct: 
+        print("HIGHER!")
+    if number == correct:
+        print("YOU WON!!!!!")
+
+    five_less = correct - 5
+    five_more = correct + 5
+
+    if number >= five_less and number <= five_more:
+        print("ALMOST! WITHIN 5 AWAY") 
